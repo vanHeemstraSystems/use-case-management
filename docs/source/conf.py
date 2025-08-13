be036a99d5e00da29cd253a99416f97dd3650fc2
Willem van Heemstra <willem@vanheemstrasystems.com>
2025-08-13 09:17:01 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
-project = 'Your Project Name'
+project = 'Use Case Management'
 copyright = '2025 - present, van Heemstra Systems'
 author = 'Willem van Heemstra'
 
